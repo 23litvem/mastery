9a0649ff63f4ca87e32003a6c45bb7059196bf8b
23litvem <59704628+23litvem@users.noreply.github.com>
2020-03-12 10:38:53 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,56 @@
+while True: 
+    firstname= input("what is your first name")
+    lastname= input("What is your last name")
+    studentid= input("what is your id number")
+    block1= input("first block ")
+    if (block1 == "stop"):
+        break
+    block2= input("second block")
+    if (block2 == "stop"):
+        break
+    block3= input("third block")
+    if (block3 == "stop"):
+        break
+    block4= input("fourth block")
+    if (block4 == "stop"):
+        break
+    block5= input("fifth block")
+    if (block5 == "stop"):
+        break
+    block6= input("sixth block")
+    if (block6 == "stop"):
+        break
+    room1= input("class 1 number")
+    room2= input("class 2 number")
+    room3= input("class 3 number")
+    room4= input("class 4 number")
+    room5= input("class 5 number")
+    room6= input("class 6 number")
+    
+    print("*************************************************")
+    print("*                                               *")
+    print("*\t",firstname,"",lastname,"\tID:",studentid,"\t*")
+    print("*                                               *")
+    print("*************************************************")
+    print("*                                               *")
+    print("*\tBlock 1:",block1,"\tRoom: " ,room1,"\t\t*")
+    print("*\tBlock 2:",block2,"\tRoom: " ,room2,"\t\t*")
+    print("*\tBlock 3:",block3,"\tRoom: " ,room3,"\t\t*")
+    print("*\tBlock 4:",block4,"\tRoom: " ,room4,"\t\t*")
+    print("*\tBlock 5:",block5,"\tRoom: " ,room5,"\t\t*")
+    print("*\tBlock 6:",block6,"\tRoom: " ,room6,"\t\t*")
+    print("*************************************************")
+    print("*                                               *")
+    print("*************************************************")
+
+
+
+
+
+
+
+
+
+
+
+    print("Mrgrimsley give me 100%")
